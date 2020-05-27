1ef655b6645af2b270810b324baf936a673991a3
Melvin Gonzalez <57917756+Melvin-Code@users.noreply.github.com>
2020-05-27 13:12:18 -0400

    checkpoint

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -31,15 +31,6 @@ console.log(jane.lastName);
 console.log(mark.lastName);
 */
 
-
-
-
-
-
-
-
-
-
 // object.create
 /*
 var personProto = {
@@ -59,7 +50,6 @@ var jane = Object.create(personProto, {
   job: { value: "designer" },
 });
 */
-
 
 // primitives vs objects
 /*
@@ -98,14 +88,123 @@ console.log(age)
 console.log(obj.city)
 */
 
+////////////////////////////////////////////////////////////////////////////////////Lecture passing functions as arguments
+
+/*
+var years = [1990, 1965, 1937, 2005, 1998];
+
+const arrayCalc = (arr, fn) => {
+    var arrRes = []
+    for (let i = 0; i < arr.length; i++){
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+};
+
+function calculateAge(el) {
+    return 2016 - el
+};
+
+
+function isFullAge(el) {
+    return el >= 18
+};
+
+function maxHeartRate(el) {
+    if (el >= 18 && el <= 81){
+    return Math.round(206.9 - (0.67 * el));
+    } else {return -1}
+}
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge)
+var rates = arrayCalc(ages, maxHeartRate)
 
 
 
+console.log(ages)
+console.log(fullAges)
+console.log(rates)
+*/
+
+////////////////////////////////////////////////////////////////////////////////////Lecture: functions returning functions
+/*
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you please explain what UX design is')
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach, ' + name + '?')
+        }
+    }else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do ?')
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+teacherQuestion('John')
+designerQuestion('jane')
+*/
+
+////////////////////////////////////////////////////////////////////////////Lecture: IIFE
+/*
+function game() {
+    var score = Math.random() * 10;
+    console.log(score >= 5)
+}
+game()
+*/
+/*
+(function () {
+    var score = Math.random() * 10;
+    console.log(score >= 5)
+})()
+
+(function (goodLuck) {
+    var score = Math.random() * 10;
+    console.log(score >= 5 - goodLuck);
+})(5)
+ */
+
+/////////////////////////////////////////////////////////////////////////////////// Lcture Closures
+
+// function retirement(retiramentAge) {
+//   var a = "years left until retirement";
+//   return function (yearOfBirth) {
+//     var age = 2016 - yearOfBirth;
+//     console.log(retiramentAge - age + a);
+//   };
+// }
+
+// var retirementUS = retirement(66);
+// retirementUS(1990);
+// var retirementGermany = retirement(65);
+// var retirementIceland = retirement(67);
+
+// retirementGermany(1990);
+// retirementIceland(1990);
+
+function interviewQuestion(job) {
+  var a = job;
+  return function (name) {
+    if (a === "designer") {
+      console.log(name + ", can you please explain what UX design is");
+    } else if (a === "teacher") {
+      console.log("What subject do you teach, " + name + "?");
+    } else {
+      console.log("Hello " + name + ", what do you do ?");
+    }
+  };
+}
 
 
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
 
-
-
-
-
-
+teacherQuestion('John')
+designerQuestion('jane')
