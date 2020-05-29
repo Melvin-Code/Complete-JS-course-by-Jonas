69bb5b28cba863c3a300329a0f31bfa1a4950667
Melvin Gonzalez <57917756+Melvin-Code@users.noreply.github.com>
2020-05-28 23:13:29 -0400

    checkpoint

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -189,22 +189,112 @@ game()
 // retirementGermany(1990);
 // retirementIceland(1990);
 
-function interviewQuestion(job) {
-  var a = job;
-  return function (name) {
-    if (a === "designer") {
-      console.log(name + ", can you please explain what UX design is");
-    } else if (a === "teacher") {
-      console.log("What subject do you teach, " + name + "?");
-    } else {
-      console.log("Hello " + name + ", what do you do ?");
-    }
-  };
-}
+// function interviewQuestion(job) {
+//   var a = job;
+//   return function (name) {
+//     if (a === "designer") {
+//       console.log(name + ", can you please explain what UX design is");
+//     } else if (a === "teacher") {
+//       console.log("What subject do you teach, " + name + "?");
+//     } else {
+//       console.log("Hello " + name + ", what do you do ?");
+//     }
+//   };
+// }
 
 
-var teacherQuestion = interviewQuestion('teacher');
-var designerQuestion = interviewQuestion('designer');
+// var teacherQuestion = interviewQuestion('teacher');
+// var designerQuestion = interviewQuestion('designer');
 
-teacherQuestion('John')
-designerQuestion('jane')
+// teacherQuestion('John')
+// designerQuestion('jane')
+
+
+/////////////////// Lecture: Bind, call and apply
+
+// var john = {
+//     name: 'John',
+//     age: 26,
+//     job: 'teacher',
+//     presentation: function(style, timeOfDay){
+//         if(style === 'formal'){
+//             console.log('Good' + timeOfDay + ', Ladies and gentlemen! I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old');
+//         }else if(style === 'friendly'){
+//             console.log('hey What\'sup?I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old')
+//         }
+//     }
+// }
+
+// var emily = {
+//     name: 'Emily',
+//     age: 35,
+//     job: 'designer'
+// };
+
+// john.presentation('formal', 'morning')
+
+// john.presentation.call(emily, 'friendly', 'afternoon');
+
+// var johnFriendly = john.presentation.bind(john, 'friendly')
+
+// johnFriendly('morning')
+// johnFriendly('nigth')
+
+// var emilyFormal = john.presentation.bind(emily, 'formal')
+// emilyFormal('afternoon')
+
+
+
+
+
+
+
+// var years = [1990, 1965, 1937, 2005, 1998];
+
+// const arrayCalc = (arr, fn) => {
+//     var arrRes = []
+//     for (let i = 0; i < arr.length; i++){
+//         arrRes.push(fn(arr[i]));
+//     }
+//     return arrRes;
+// };
+
+// function calculateAge(limit) {
+//     return 2016 - limit
+// };
+
+
+// function isFullAge(limit, el) {
+//     return el >= limit
+// };
+
+// var ages = arrayCalc(years, calculateAge)
+// var fulJapan = arrayCalc(ages, isFullAge.bind(this, 20))
+// console.log(ages)
+// console.log(fulJapan)
+
+/*
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constructor
+
+3. Store them all inside an array
+
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+*/
+
+
+
+  
+
+
+
